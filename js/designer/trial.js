195a76a34cfba11c0f8432b54f80385a5654f1d1
Nikita Bragin <nikita-bragin@mail.ru>
2015-06-02 19:06:52 +0300

    Delete trial.js

--- js/designer/trial.js
+++
@@ -1,8 +0,0 @@
-var DEJS;
-
-(function (DEJS) {
-    (function (Trial) {
-        Trial.isFull = true;
-    })(DEJS.Trial || (DEJS.Trial = {}));
-    var Trial = DEJS.Trial;
-})(DEJS || (DEJS = {}));
