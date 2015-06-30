6e4ad554879bc36c8c89ba071401a38b8cc5fc75
Nikita Bragin <nikita-bragin@mail.ru>
2015-06-30 11:47:02 -0700

    Fonts colors palette update

--- js/ui.js
+++ js/ui.js
@@ -811,7 +811,10 @@ function DEControlsModel() {
         event.preventDefault();
         self.selectedProductElementColor(colorizeElement);
         self.colorsList(colorsList);
+        self.setColorsByGroups(colorsList);
+    };
 
+    self.setColorsByGroups = function (colorsList) {
         //----- for colors palette in mobile version where it is devided in groups by 9
         var i = 0,
             k = 0,
@@ -830,7 +833,6 @@ function DEControlsModel() {
         if (group.length !== 0) {
             self.colorsGroupsList.push({items: group});
         }
-        //-----
     };
 
     self.colorSelected = function (color, event) {
@@ -1663,6 +1665,12 @@ function DEControlsModel() {
     });
 
     self.showFontsList = ko.observable(false);
+    //strokeColors
+
+    self.showFontsColorsListMobile = function () {
+        self.setColorsByGroups(self.colors());
+        self.colorsList(self.colors());
+    };
 
     self.toggleFontsList = function (model, event) {
         var currentValue = self.showFontsList();
